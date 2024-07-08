308c67725e39c785c8b1e8078af4d49f4151591f
kaavehcodes <kaaveh.rostami@gmail.com>
2024-07-08 17:38:01 +0330

    Modify icons style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,12 @@ const config: Config = {
         white: "#EFF1F3",
         timberwolf: "#DBD3D8",
         sand: "#D8B4A0",
-        burnt: "#D77A61"
+        burnt: "#D77A61",
+        twitter: "#1DA1F2",
+        dribbbledark: "#C32361",
+        dribbblepink: "#EA4C89",
+        github: "#1F2328",
+        linkedin: "#2867B2"
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
