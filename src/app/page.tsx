a297a9a2e7e6af78dc236e93617758b02f564790
kaavehcodes <kaaveh.rostami@gmail.com>
2024-07-13 21:47:51 +0330

    Add cta section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
 import { SKILLS } from "@/content/data-skills";
 
+import Image from "next/image";
+import Link from "next/link";
+
 import Logo from "@/components/Logo";
 import LogoType from "@/components/LogoType";
 import MainMenu from "@/components/MainMenu";
 import Navbar from "@/components/Navbar";
 import Tableau from "@/components/Tableau";
 import SkillTag from "@/components/SkillTag";
-import Image from "next/image";
-import Link from "next/link";
+
 import FormContact from "@/components/FormContact";
 import BlogPost from "@/components/BlogPost";
+import IconDribbble from "@/components/icons/IconDribbble"
+import IconGithub from "@/components/icons/IconGithub"
+import IconLinkedin from "@/components/icons/IconLinkedin"
+import IconTwitter from "@/components/icons/IconTwitter"
 
 export default function Home() {
   return (
@@ -105,6 +111,40 @@ export default function Home() {
           <BlogPost />
         </div>
       </section>
+      <section className="container w-1/2 mx-auto">
+        <div className="border-2 border-black bg-black px-16 py-8">
+          <h2 className="text-2xl font-light text-white mb-2">Start a Project</h2>
+          <p className="text-xl font-thin text-white mb-4">Interested in working together?</p>
+          <button className="border-2 border-black bg-white text-xl font-light px-8 py-4">Lets do this</button>
+        </div>
+      </section>
+      <footer>
+        <figure>Glasses</figure>
+        <p>Slogan</p>
+        <ul className="flex flex-row items-center">
+          <li className="mr-2">
+            <a href="https://x.com/KaavehTweets" className="cursor-pointer" target="_blank" no-referrer>
+              <IconTwitter width={32} height={32} />
+            </a>
+          </li>
+          <li className="mr-2">
+            <a href="https://dribbble.com/kaavehdesigns" className="cursor-pointer" target="_blank" no-referrer>
+              <IconDribbble width={32} height={32} />
+            </a>
+          </li>
+          <li className="mr-2">
+            <a href="https://github.com/kaavehcodes" className="cursor-pointer" target="_blank" no-referrer>
+              <IconGithub width={32} height={32} />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/kaavehrostami/" className="cursor-pointer" target="_blank" no-referrer>
+              <IconLinkedin width={32} height={32} />
+            </a>
+          </li>
+        </ul>
+        <p>Copyright</p>
+      </footer>
     </main>
   );
 }
