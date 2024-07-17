ef47306970699604b5f095d220e7623ee2d67331
kaavehcodes <kaaveh.rostami@gmail.com>
2024-07-17 12:06:35 +0330

    Modify works page

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,6 +1,7 @@
 import { works } from "#site/content"
-import { sortPosts } from "@/lib/utils"
+import { cn, sortPosts } from "@/lib/utils"
 import BlogPost from "@/components/BlogPost"
+import Header from "@/components/Header"
 
 export default async function Works() {
   const sortedWorks = sortPosts(works.filter((work) => work.published))
@@ -8,10 +9,29 @@ export default async function Works() {
 
   return (
     <main>
-      <section>
-        <div>
-          <h1>Blog</h1>
+      <Header page="works" />
+      <section className="flex flex-row items-end border-b-2 my-16">
+        <div className="px-8 mr-32">
+          <h1 className="text-8xl font-medium">Works</h1>
         </div>
+        <div className="">
+          <ul className="flex flex-row items-center">
+            <li className="text-2xl font-medium border-b-2 border-black px-4">
+              all
+            </li>
+            <li className="text-2xl font-light px-4">
+              branding
+            </li>
+            <li className="text-2xl font-light px-4">
+              product
+            </li>
+            <li className="text-2xl font-light px-4">
+              games
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section className="container mx-auto">
         <div>
           {displayWorks?.length > 0 ? (
             <ul className="flex flex-col">
